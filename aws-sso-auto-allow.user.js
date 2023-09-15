dcb9a1064bb3812cf845eb2667319bc546ba0eef
Alyssa Kinley <akinley@protonmail.com>
2023-09-14 22:05:28 -0700

    Update AWS CLI SSO automation

--- aws-sso-auto-allow.user.js
+++ aws-sso-auto-allow.user.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name        AWS SSO - Auto Allow
-// @version     1.0
+// @version     1.1
 // @namespace   none
 // @grant       none
 // @run-at      document-idle
+// @match       https://device.sso.*.amazonaws.com/
 // @match       https://*.awsapps.com/start/user-consent/authorize.html
 // ==/UserScript==
 
+// 1. Confirm and continue
+document.getElementById("cli_verification_btn")?.click()
+
+// 2. Allow
 document.getElementById("cli_login_button")?.click()
